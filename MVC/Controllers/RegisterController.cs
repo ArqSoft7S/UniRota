451f7f595d7b85d5ceaa0b3dfd3d216d0c794fb4
Thiago Arcoverde <tluiz2701@gmail.com>
2024-03-06 20:35:02 -0300

    Criação inicial de função de registro + config banco e migration

--- MVC/Controllers/RegisterController.cs
+++ MVC/Controllers/RegisterController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+using MVC.Models;
 
 namespace MVC.Controllers
 {
     public class RegisterController : Controller
     {
+
+        private readonly ApplicationDbContext _context; 
+
+        public RegisterController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View("Register");
+        }
+
+        public async Task<IActionResult> RegistrarUsuario()
+        {
+            return View("~/Views/Login/Login.cshtml");
         }
     }
 }
---
+++ MVC/Data/Migrations/20240306232151_CriadoTabelaDeUsuarios.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriadoTabelaDeUsuarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    Senha = table.Column<string>(type: "TEXT", nullable: true),
+                    NumeroCelular = table.Column<string>(type: "TEXT", nullable: true),
+                    CEP = table.Column<string>(type: "TEXT", nullable: true),
+                    Rua = table.Column<string>(type: "TEXT", nullable: true),
+                    Numero = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cidade = table.Column<string>(type: "TEXT", nullable: true),
+                    Complemento = table.Column<string>(type: "TEXT", nullable: true),
+                    Bairro = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
