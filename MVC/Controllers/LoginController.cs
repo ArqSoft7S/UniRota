dc7376ec527103223d487269c1e77af6496efa45
Thiago Arcoverde <tluiz2701@gmail.com>
2024-03-07 00:04:42 -0300

    Adicionado funcao de login e registro

--- MVC/Controllers/LoginController.cs
+++ MVC/Controllers/LoginController.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Data;
 
 namespace MVC.Controllers
 {
     public class LoginController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public LoginController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            return View("Login");
+        }
+
+        public IActionResult LogarUsuario(string InputEmail, string InputPassword)
+        {
+            var user = _context.Usuarios.Where(w => w.Email == InputEmail && w.Senha == InputPassword).FirstOrDefault();
+            if (user == null)
+            {
+                return View("~/Views/Home/Index.cshtml");
+            }
             return View("Login");
         }
     }
--- MVC/Controllers/RegisterController.cs
+++ MVC/Controllers/RegisterController.cs
@@ -8,7 +8,7 @@ namespace MVC.Controllers
     public class RegisterController : Controller
     {
 
-        private readonly ApplicationDbContext _context; 
+        private readonly ApplicationDbContext _context;
 
         public RegisterController(ApplicationDbContext context)
         {
@@ -20,9 +20,30 @@ namespace MVC.Controllers
             return View("Register");
         }
 
-        public async Task<IActionResult> RegistrarUsuario()
+        [HttpPost]
+        public IActionResult RegistrarUsuario(string InputEmail, string InputPassword, string InputPasswordConfirm,
+            string InputCellPhone, string InputCep, string InputRua, string InputNumber, string InputComplement,
+            string inputNeighborhood, bool Check)
         {
+            if (!String.Equals(InputPassword, InputPasswordConfirm) || !Check)
+                return View("Register");
+            if (_context.Usuarios.Where(w => w.Email == InputEmail).Any())
+                return View("Register");
+
+            _context.Usuarios.Add(new UsuarioModel
+            {
+                Email = InputEmail,
+                Senha = InputPassword,
+                NumeroCelular = InputCellPhone,
+                CEP = InputCep,
+                Rua = InputRua,
+                Numero = int.Parse(InputNumber),
+                Complemento = InputComplement,
+                Bairro = inputNeighborhood
+            });
+            _context.SaveChanges();
             return View("~/Views/Login/Login.cshtml");
+
         }
     }
 }
